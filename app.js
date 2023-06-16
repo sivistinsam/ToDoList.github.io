50a902a7dbfa359375d11ec33d3a0d06fbad4245
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-06-16 15:58:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,101 +1,93 @@
-const input = document.querySelector('#input');
-const addBtn = document.querySelector('#add');
-const tasks = document.querySelector('.tasks');
-const taskCount = document.querySelector('#spanNumber');
+// Defining variable to access all the class from html
+const input = document.querySelector("#input");
+// Input field for user name
+const addBtn = document.querySelector("#add");
+// Add button in HTML file
+const tasks = document.querySelector(".tasks");
+// Tasks div element
+const taskCount = document.querySelector("#spanNumber");
+// Creating a function that will be called when we click on "Add" Button
+const cancel = document.querySelector(".fa-xmark");
+// Cancel icon
+const strike = document.querySelector(".fa-strikethrough");
+// Strike through icon
 
-const cancel = document.querySelector('.fa-xmark');
-const strike = document.querySelector('.fa-strikethrough');
-
-
-input.addEventListener('keyup', () => {
-    if (input.value.trim() != 0) {
-        addBtn.classList.add('active');
-    }
-    else {
-        addBtn.classList.remove('active');
-    }
+input.addEventListener("keyup", () => {
+  // When keyup event is triggered, it checks if there's any value entered or not and
+  if (input.value.trim() != 0) {
+    // If yes then create an li tag with text as what ever was written by the user
+    addBtn.classList.add("active");
+  } else {
+    // Else remove active class of add btn so that no new list item gets added without entering anything
+    addBtn.classList.remove("active");
+  }
 });
-
-
+// creating array for storing user input todos
 let newTaskList = [];
+// Creating a function to add all the input which was entered by the user
 function addList() {
-
-    let pos = tasks.firstElementChild;
-    if (input.value.trim() != 0) {
-        let newTask = document.createElement('div');
-        newTask.classList.add('item');
-        newTask.innerHTML = `
-        
+  let pos = tasks.firstElementChild;
+  //   This if conditon will check if input field is not empty then it will proceed further to add a task
+  if (input.value.trim() != 0) {
+    // Creating a new div to display the todo items
+    let newTask = document.createElement("div");
+    // Adding classes to each elements created above
+    newTask.classList.add("item");
+    // Appending heading as input of the user and buttons for the strike through and delete
+    newTask.innerHTML = `
         <h5>${input.value}</h5>
             <div class="btn">
                 <i class="fa-sharp fa-solid fa-strikethrough"></i>
                 <i class="fa-solid fa-xmark"></i>
-
             </div>
-            
-        `
-
-        if (pos == null) {
-            tasks.appendChild(newTask);
-        }
-        else {
-            tasks.insertBefore(newTask, pos);
-        }
-
-        newTaskList.push(newTask);
-        count();
-        // const dataFromLocal = localStorage.getItem(input.value);
-        // console.log("dataFromLocal", dataFromLocal);
-        // let ram = input.value;
-        // const tom = localStorage.setItem("listName", JSON.stringify(ram));
-        // const rom = localStorage.getItem("listName");
-        // const parsedMovieData = JSON.parse(rom);
-        // console.log(parsedMovieData);
-
-
-
-
-
-
-
-
-        input.value = '';
+        `;
+    // Pushing newly created object into our array
+    if (pos == null) {
+      // checking if pos is empty or not
+      tasks.appendChild(newTask);
+    } else {
+      // adding after first child node
+      tasks.insertBefore(newTask, pos);
     }
-    else {
-        alert('Please enter a task :)')
-    }
+    // Clearing out the input box once done
 
+    newTaskList.push(newTask);
+    // Updating number span count
+    count();
+    // Calling update counter method
+
+    input.value = "";
+  } else {
+    alert("Please enter a task :)");
+  }
 }
+// creating function for counting the how many list is present in todo
 
 function count() {
-    taskCount.innerHTML = newTaskList.length;
+  taskCount.innerHTML = newTaskList.length;
 }
+// Function to handle click event on Add button
 
-
-
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", () => {
+  addList();
+});
+// creating function to add the input of the user by pressing enter button
+input.addEventListener("keyup", (event) => {
+  if (event.which == 13) {
     addList();
+  }
 });
-
-input.addEventListener('keyup', (event) => {
-    if (event.which == 13) {
-        addList();
-    }
+// Strikethrough functionality starts here
+tasks.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-xmark")) {
+    e.target.parentElement.parentElement.remove();
+    newTaskList.pop(newTaskList.newTask);
+    count();
+  }
 });
-
-tasks.addEventListener('click', (e) => {
-    if (e.target.classList.contains('fa-xmark')) {
-        e.target.parentElement.parentElement.remove();
-        newTaskList.pop(newTaskList.newTask);
-        count();
-
-    }
+// Checking completed items and changing their color accordingly
+tasks.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-strikethrough")) {
+    e.target.parentElement.parentElement.classList.toggle("completed");
+  }
 });
-
-tasks.addEventListener('click', (e) => {
-    if (e.target.classList.contains('fa-strikethrough')) {
-        e.target.parentElement.parentElement.classList.toggle('completed');
-    }
-});
-
-
